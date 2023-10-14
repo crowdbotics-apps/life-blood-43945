41ae2d09a22d94e82b3877feeecd2aada8ded9f6
Crowdbotics <team@crowdbotics.com>
2023-10-14 22:51:42 +0000

    Initial commit

---
+++ backend/life_blood_43945/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for life_blood_43945 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "life_blood_43945.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "life-blood-43945",
+  url: "https://life-blood-43945.botics.co",
+  api: "https://life-blood-43945.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
